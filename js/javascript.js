4f2933e83c6344adfd3a5051d99b769f83ef5df0
Vyacheslav <v.golyaka@ukr.net>
2018-03-14 14:39:47 +0200

    Add script for back-to-top button

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,4 @@
-// googlemap functions
+// --- googlemap functions
 function myMap1() {
   var myCenterDUBR1 = new google.maps.LatLng(51.571727, 26.567370);
   var mapCanvas = document.getElementById("googlemap1");
@@ -14,6 +14,27 @@ function myMap1() {
   marker.setMap(map)
 }
 
+// --- for back-to-top button
+window.onscroll = function() {
+	scrollFunction()
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.getElementById("back-to-top").style.display = "block";
+	} else {
+		document.getElementById("back-to-top").style.display = "none";
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//     document.body.scrollTop = 0; // For Safari
+//     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }
+
+
+// --- Example Buttons
 function changeTextColor(textColor, borderColor) {
 	var elem = document.getElementById('textresult');
 	elem.style.color = textColor;
