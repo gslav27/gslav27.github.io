e79dcdc061c53ac50bae08257ae366cbf497fb07
Vyacheslav <v.golyaka@ukr.net>
2018-11-21 11:56:15 +0200

    Update javascript.js

--- other/js/javascript.js
+++ other/js/javascript.js
@@ -3,7 +3,7 @@
 var button = document.getElementById( 'menu-toggle' );
 
 // Click the button.
-button.onclick = function() {
+button && button.onclick = function() {
   // Toggle class "opened". Set also aria-expanded to true or false.
   if ( -1 !== button.className.indexOf( 'opened' ) ) {
     button.className = button.className.replace( ' opened', '' );
